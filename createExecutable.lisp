c0788ab4b38304dd27509feded65fa334f7f469c
crunsk <vrranta@gmail.com>
2020-04-13 14:31:11 +0300

    Set *default-external-format* to utf-8 when creating executable

--- createExecutable.lisp
+++ createExecutable.lisp
@@ -1,3 +1,4 @@
+(setf SB-IMPL::*DEFAULT-EXTERNAL-FORMAT* :utf-8) ;;This was added so there's no need to change file encoding when developing on Windows
 (load "packages.lisp")
 (load "pesukonevahti.lisp")
 #+:OS-WINDOWS (save-lisp-and-die "vahti.exe" :toplevel #'pesukonevahti:tarkasta :executable t)

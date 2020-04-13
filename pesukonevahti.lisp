ab5bb896f27acf81623f36e2b548fcea89339e10
crunsk <vrranta@gmail.com>
2020-04-13 14:04:21 +0300

    rename variables in tarkasta function

--- pesukonevahti.lisp
+++ pesukonevahti.lisp
@@ -76,14 +76,12 @@
 	(vs3)
 	(vsg3)
 	(ev3)
-	(*request*)
-	(*parsed-content*)
-	(*login*)
-	(*parsed-content-login*)
-	(*request-varaa*)
-	(*parsed-content-varaa*)
-	(*request-ajat*)
-	(*parsed-content-ajat*)
+	(request)
+	(parsed-content)
+	(login)
+	(parsed-content-login)
+	(request-varaa)
+	(parsed-content-varaa)
 	(has-credentials))
     
     (tallenna-kirjautumistiedot)
@@ -95,43 +93,43 @@
 	  (setf *mixer* (mixalot:create-mixer))
 
 	  ;;pyydetään aloitussivu
-	  (setf *request*
+	  (setf request
 		(dex:post "https://ilmarinen.vuorosi.fi/Default.aspx"
 			  :content (acons "ctl00$ContentPlaceHolder1$tbPassword" *password* (acons "ctl00$ContentPlaceHolder1$tbUsername" *username* '())) :cookie-jar *cookie-jar*))
 	  ;;parsetaan sisältö
-	  (setf *parsed-content* (lquery:$ (initialize *request*)))
+	  (setf parsed-content (lquery:$ (initialize request)))
 	  
 	  
-	  (setf vs (aref (lquery-funcs:val (lquery:$ *parsed-content* "#__VIEWSTATE")) 0))
-	  (setf vsg (aref (lquery:$ *parsed-content* "#__VIEWSTATEGENERATOR" (attr :value)) 0 ))
-	  (setf ev (aref (lquery-funcs:val (lquery:$ *parsed-content* "#__EVENTVALIDATION")) 0))
+	  (setf vs (aref (lquery-funcs:val (lquery:$ parsed-content "#__VIEWSTATE")) 0))
+	  (setf vsg (aref (lquery:$ parsed-content "#__VIEWSTATEGENERATOR" (attr :value)) 0 ))
+	  (setf ev (aref (lquery-funcs:val (lquery:$ parsed-content "#__EVENTVALIDATION")) 0))
 	  
 					;tallennan sisäänkirjautumisrequestin komennolla
-	  (setf *login* (fun-dex-post-contents vs vsg ev))
-					;parsetaan uusi request *login* muuttujasta komennolla
-	  (setf *parsed-content-login* (lquery:$ (initialize *login*)))
+	  (setf login (fun-dex-post-contents vs vsg ev))
+					;parsetaan uusi request login muuttujasta komennolla
+	  (setf parsed-content-login (lquery:$ (initialize login)))
 	  
 					;Haetaan uudet parametrien arvot komennolla
-	  (setf vs2 (aref (lquery-funcs:val (lquery:$ *parsed-content-login* "#__VIEWSTATE")) 0))
-	  (setf vsg2 (aref (lquery:$ *parsed-content-login* "#__VIEWSTATEGENERATOR" (attr :value)) 0 ))
-	  (setf ev2 (aref (lquery-funcs:val (lquery:$ *parsed-content-login* "#__EVENTVALIDATION")) 0))
+	  (setf vs2 (aref (lquery-funcs:val (lquery:$ parsed-content-login "#__VIEWSTATE")) 0))
+	  (setf vsg2 (aref (lquery:$ parsed-content-login "#__VIEWSTATEGENERATOR" (attr :value)) 0 ))
+	  (setf ev2 (aref (lquery-funcs:val (lquery:$ parsed-content-login "#__EVENTVALIDATION")) 0))
 	  
 	  
 					;tein funktion, jolla pääsee portaaliin, kun tätä funktiota kutsuu muuttujilla vs2 vsg2 ev2
 	  
 	  
-					;tallensin kutsun tuloksen muuttujaan *request-varaa*
+					;tallensin kutsun tuloksen muuttujaan request-varaa
 	  
-	  (setf *request-varaa* (fun-dex-post-portal-status vs2 vsg2 ev2))
+	  (setf request-varaa (fun-dex-post-portal-status vs2 vsg2 ev2))
 	  
 					;parsetaan request-varaa
-	  (setf *parsed-content-varaa* (lquery:$ (initialize *request-varaa*)))
+	  (setf parsed-content-varaa (lquery:$ (initialize request-varaa)))
 	  (format t "Verrataan ajanhetkellä~%")
 	  (let ((aika (multiple-value-list (decode-universal-time (get-universal-time) -2))))
 	    (format t "~a:~a ~a.~a.~a~%" 
 		    (nth 2 aika) (nth 1 aika) (nth 3 aika) (nth 4 aika) (nth 5 aika)))
 ;;;    (format t "usr: ~a pass: ~a" *username* *password*)
-	  (if (not (search "käynn" (string-downcase (aref (lquery:$ *parsed-content-varaa* "#ctl00_ContentPlaceHolder1_Repeater1_ctl00_Repeater2_ctl00_MaskGrpTitle" (text)) 0)))
+	  (if (not (search "käynn" (string-downcase (aref (lquery:$ parsed-content-varaa "#ctl00_ContentPlaceHolder1_Repeater1_ctl00_Repeater2_ctl00_MaskGrpTitle" (text)) 0)))
 		   ) (progn
 		       (format t "Kone vapaana!~%")
 		       (mixalot:mixer-add-streamer *mixer*
